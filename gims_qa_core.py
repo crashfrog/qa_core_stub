ea82eeefacaa0a3ac477d2efb9a74d038902bcf7
Justin Payne <justin.payne@fda.hhs.gov>
2015-05-15 14:57:13 -0400

    working build

--- gims_qa_core.py
+++ gims_qa_core.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import xml.dom.minidom as dom
 import xml.etree.ElementTree as xml
 import sys
 import os, os.path
@@ -8,6 +9,9 @@ from inspect import getargspec, getfile, currentframe
 from os.path import join as j
 import subprocess
 import textwrap
+import imp
+from collections import OrderedDict
+
 
 usage = """ CFSAN Genomics QA Core 0.1
 May 15, 2015
@@ -20,21 +24,22 @@ justin.payne@fda.hhs.gov
 
 """
 
-qa_tests = dict()
-qa_undecorated_tests = dict()
+
+
+qa_tests = OrderedDict()
+qa_undecorated_tests = OrderedDict()
 
 def qa_test(qa_func):
 	"Decorator to simplify XML interaction"
 	qa_undecorated_tests[qa_func.__name__] = qa_func
-	def bind_metadata_to_arguments(qa_func, metadata):
+	def bind_metadata_to_arguments(metadata, qa_func):
 		"Find elements in XML with same name as function argument and bind their content"
-		for args, varargs, keywords, defaults in getargspec(qa_func):
-			for argument in args:
-				elements = metadata.findall(".//{0}".format(argument))
-				if len(elements) == 1:
-					qa_func = partial(qa_func, **{argument:elements[0].text})
-				else:
-					qa_func = partial(qa_func, **{argument:[e.text for e in elements]})
+		for argument in getargspec(qa_func).args:
+			elements = metadata.findall(".//{0}".format(argument))
+			if len(elements) == 1:
+				qa_func = partial(qa_func, **{argument:elements[0].text})
+			else:
+				qa_func = partial(qa_func, **{argument:[e.text for e in elements]})
 		return qa_func()
 		
 		
@@ -42,27 +47,33 @@ def qa_test(qa_func):
 	qa_tests[qa_func.__name__] = decorated_qa_func
 	return decorated_qa_func
 
+qa_core = imp.new_module("qa_core")
+qa_core.qa_test = qa_test
+sys.modules['qa_core'] = qa_core
 	
 
 def main(sandbox_path=os.getcwd()):
 	with open(j(sandbox_path, 'Input', 'input.xml'), 'rU') as inputfile:
-		metadata = xml.parse(inputfile).root()
+		metadata = xml.parse(inputfile).getroot()
 	
 	qaoutput = xml.Element("qaQcCoverageOutput")
 	outputxml = xml.ElementTree(element=qaoutput)
 	
 	for name, test in qa_tests.items():
-		xml.SubElement(qaoutput, name).text = test(metadata)
+		xml.SubElement(qaoutput, name).text = str(test(metadata))
 		
 	with open(j(sandbox_path, 'Output', 'output.xml'), 'w') as outputfile:
-		outputxml.write(outputfile, encoding="UTF-8", xml_declaration=True)
+		d = dom.parseString(xml.tostring(qaoutput))
+		d.writexml(outputfile, addindent='    ', newl='\n')
+# 		outputxml.write(outputfile, encoding="UTF-8", xml_declaration=True)
 
 @qa_test
 def version():
 	"Collect repository commit id from git"
 	p = os.path.dirname(__file__)
-	with open(j(p, '.git/refs/heads/master'), 'rU') as head_ref:
-		ref = head_ref.read().replace('\n', '')
+#	 with open(j(p, '.git/refs/heads/master'), 'rU') as head_ref:
+# 		ref = head_ref.read().replace('\n', '')
+	ref = subprocess.check_output("git -C {0} describe".format(p), shell=True).replace("\n", '')
 	return ref
 
 
@@ -84,7 +95,6 @@ if __name__ == "__main__":
 		main(sandbox_path)
 	except ValueError:
 		print("Missing argument: path to sandbox or name of QA test.")
-		
 		commands = " | ".join([':'.join((n, ",".join(["<{0}>".format(a) for a in getargspec(f)[0]]) or 'DELETEME')).replace(':DELETEME', '') for n, f in qa_undecorated_tests.items()])
 		print(usage.format(os.path.basename(__file__), textwrap.fill(commands, 79, subsequent_indent='\t\t')))
 		quit()
--- gims_qa_tests.py
+++ gims_qa_tests.py
@@ -1,5 +1,21 @@
-from gims_qa_core import qa_test
+from qa_core import qa_test
+
+
+
 
 @qa_test
-def another_test():
-	pass
+def readCount(sequencingFileName, sequencingType):
+	return 100
+	
+@qa_test
+def coverage():
+	return 100
+	
+# @qa_test
+# def coverageQThirtyPlus():
+# 	return 100
+
+@qa_test	
+def qcStatus(coverageThreshold=None):
+	return "Pass"
+	
